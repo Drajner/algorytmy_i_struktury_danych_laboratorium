66ccbc6ac21f43990d54abe55c711956fdaee2ad
Michal <mpkowalcz@wp.pl>
2021-03-28 19:52:48 +0200

    Implemented plotting function, fixed an issue with quick_sort benchmarking

--- lab2/sorts_benchmark.py
+++ lab2/sorts_benchmark.py
@@ -1,6 +1,7 @@
 from sorts import bubble_sort, selection_sort, quick_sort, merge_sort
 import os
 import timeit
+from matplotlib import pyplot
 
 
 def get_table_of_words(num_of_words):
@@ -20,26 +21,38 @@ def get_table_of_words(num_of_words):
     return table
 
 
-def get_times():
-    print('dupa')
+def get_times(iter_times=1):
     tests_not_finished = True
     num_of_words = 1000
     results = {'Bubble': [], 'Selection': [], 'Merge': [], 'Quick': []}
     while(tests_not_finished):
-        table_of_words = get_table_of_words(num_of_words)
-        results['Bubble'].append(timeit.timeit('sorts.bubble_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=1))
-        print(results['Bubble'])
-        results['Selection'].append(timeit.timeit('sorts.selection_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=1))
-        print(results['Selection'])
-        results['Merge'].append(timeit.timeit('sorts.merge_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=1))
-        print(results['Merge'])
-        results['Quick'].append(timeit.timeit('sorts.quick_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=1))
-        print(results['Quick'])
+        results['Bubble'].append(timeit.timeit('sorts.bubble_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=iter_times))
+        results['Selection'].append(timeit.timeit('sorts.selection_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=iter_times))
+        results['Merge'].append(timeit.timeit('sorts.merge_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=iter_times))
+        quick_temp_table = []
+        for i in range(1, iter_times):
+            quick_temp_table.append(timeit.timeit('sorts.quick_sort(table)', setup="import sorts; from __main__ import get_table_of_words; num = %i;table = get_table_of_words(num)" % (num_of_words,), number=1))
+        results['Quick'].append(sum(quick_temp_table))
         num_of_words += 1000
-        print(num_of_words)
         if num_of_words > 10000:
             tests_not_finished = False
     return results
 
 
-result = get_times()
+def plot_data(x_axis, y_axis, x_label="", y_label=""):
+    pyplot.plot(x_axis, y_axis)
+    pyplot.ylabel(y_label)
+    pyplot.xlabel(x_label)
+
+
+if __name__ == "__main__":
+    iter_times = 10
+    results = get_times(iter_times)
+    for name in results:
+        y_axis = results[name]
+        x_axis = [i*1000 for i in range(1, 11)]   
+        x_label = "Liczba sortowanych słów"
+        y_label = "Czas [s] posortowania n razy za pomocą " + name.lower() + " sort" + f" [n={iter_times}]"
+        plot_data(x_axis, y_axis, x_label, y_label)
+        pyplot.savefig(name + ".png")
+        pyplot.clf()
